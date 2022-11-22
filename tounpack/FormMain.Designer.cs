171f061feba86db2342b1ca37d777bb993587a4c
talmes1502 <99317254+talmes1502@users.noreply.github.com>
2022-11-22 20:03:18 +0200

    Add files via upload

---
+++ tounpack/FormMain.Designer.cs
@@ -0,0 +1,251 @@
+﻿namespace BasicFacebookFeatures
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonLogin = new System.Windows.Forms.Button();
+            this.buttonLogout = new System.Windows.Forms.Button();
+            this.checkBoxRememberMe = new System.Windows.Forms.CheckBox();
+            this.pictureBoxCover = new System.Windows.Forms.PictureBox();
+            this.pictureBoxProfile = new System.Windows.Forms.PictureBox();
+            this.listBoxFriends = new System.Windows.Forms.ListBox();
+            this.buttonShowAllFriends = new System.Windows.Forms.Button();
+            this.listBoxPosts = new System.Windows.Forms.ListBox();
+            this.listBoxPostComments = new System.Windows.Forms.ListBox();
+            this.labelCurrentPostLikes = new System.Windows.Forms.Label();
+            this.listBoxPages = new System.Windows.Forms.ListBox();
+            this.listBoxGroups = new System.Windows.Forms.ListBox();
+            this.listBoxEvents = new System.Windows.Forms.ListBox();
+            this.listBoxAbout = new System.Windows.Forms.ListBox();
+            this.listBoxCheckIns = new System.Windows.Forms.ListBox();
+            this.pictureBoxPage = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxCover)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxProfile)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxPage)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonLogin
+            // 
+            this.buttonLogin.Location = new System.Drawing.Point(12, 12);
+            this.buttonLogin.Name = "buttonLogin";
+            this.buttonLogin.Size = new System.Drawing.Size(179, 23);
+            this.buttonLogin.TabIndex = 36;
+            this.buttonLogin.Text = "Login";
+            this.buttonLogin.UseVisualStyleBackColor = true;
+            this.buttonLogin.Click += new System.EventHandler(this.buttonLogin_Click);
+            // 
+            // buttonLogout
+            // 
+            this.buttonLogout.Location = new System.Drawing.Point(12, 40);
+            this.buttonLogout.Name = "buttonLogout";
+            this.buttonLogout.Size = new System.Drawing.Size(179, 23);
+            this.buttonLogout.TabIndex = 52;
+            this.buttonLogout.Text = "Logout";
+            this.buttonLogout.UseVisualStyleBackColor = true;
+            this.buttonLogout.Click += new System.EventHandler(this.buttonLogout_Click);
+            // 
+            // checkBoxRememberMe
+            // 
+            this.checkBoxRememberMe.Location = new System.Drawing.Point(12, 69);
+            this.checkBoxRememberMe.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.checkBoxRememberMe.Name = "checkBoxRememberMe";
+            this.checkBoxRememberMe.Size = new System.Drawing.Size(107, 17);
+            this.checkBoxRememberMe.TabIndex = 53;
+            this.checkBoxRememberMe.Text = "Remember Me";
+            this.checkBoxRememberMe.UseVisualStyleBackColor = true;
+            // 
+            // pictureBoxCover
+            // 
+            this.pictureBoxCover.Location = new System.Drawing.Point(252, 11);
+            this.pictureBoxCover.Margin = new System.Windows.Forms.Padding(2);
+            this.pictureBoxCover.Name = "pictureBoxCover";
+            this.pictureBoxCover.Size = new System.Drawing.Size(337, 85);
+            this.pictureBoxCover.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBoxCover.TabIndex = 54;
+            this.pictureBoxCover.TabStop = false;
+            // 
+            // pictureBoxProfile
+            // 
+            this.pictureBoxProfile.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBoxProfile.Location = new System.Drawing.Point(379, 46);
+            this.pictureBoxProfile.Margin = new System.Windows.Forms.Padding(2);
+            this.pictureBoxProfile.Name = "pictureBoxProfile";
+            this.pictureBoxProfile.Size = new System.Drawing.Size(80, 92);
+            this.pictureBoxProfile.TabIndex = 55;
+            this.pictureBoxProfile.TabStop = false;
+            // 
+            // listBoxFriends
+            // 
+            this.listBoxFriends.FormattingEnabled = true;
+            this.listBoxFriends.Location = new System.Drawing.Point(12, 140);
+            this.listBoxFriends.Margin = new System.Windows.Forms.Padding(2);
+            this.listBoxFriends.Name = "listBoxFriends";
+            this.listBoxFriends.Size = new System.Drawing.Size(112, 43);
+            this.listBoxFriends.TabIndex = 56;
+            // 
+            // buttonShowAllFriends
+            // 
+            this.buttonShowAllFriends.Location = new System.Drawing.Point(12, 186);
+            this.buttonShowAllFriends.Margin = new System.Windows.Forms.Padding(2);
+            this.buttonShowAllFriends.Name = "buttonShowAllFriends";
+            this.buttonShowAllFriends.Size = new System.Drawing.Size(111, 18);
+            this.buttonShowAllFriends.TabIndex = 57;
+            this.buttonShowAllFriends.Text = "Show all friends";
+            this.buttonShowAllFriends.UseVisualStyleBackColor = true;
+            this.buttonShowAllFriends.Click += new System.EventHandler(this.buttonShowFriends_Click);
+            // 
+            // listBoxPosts
+            // 
+            this.listBoxPosts.FormattingEnabled = true;
+            this.listBoxPosts.Location = new System.Drawing.Point(138, 140);
+            this.listBoxPosts.Margin = new System.Windows.Forms.Padding(2);
+            this.listBoxPosts.Name = "listBoxPosts";
+            this.listBoxPosts.Size = new System.Drawing.Size(439, 82);
+            this.listBoxPosts.TabIndex = 58;
+            this.listBoxPosts.SelectedIndexChanged += new System.EventHandler(this.listBoxPosts_SelectedIndexChanged);
+            // 
+            // listBoxPostComments
+            // 
+            this.listBoxPostComments.FormattingEnabled = true;
+            this.listBoxPostComments.Location = new System.Drawing.Point(637, 153);
+            this.listBoxPostComments.Margin = new System.Windows.Forms.Padding(2);
+            this.listBoxPostComments.Name = "listBoxPostComments";
+            this.listBoxPostComments.Size = new System.Drawing.Size(113, 69);
+            this.listBoxPostComments.TabIndex = 59;
+            // 
+            // labelCurrentPostLikes
+            // 
+            this.labelCurrentPostLikes.Location = new System.Drawing.Point(638, 140);
+            this.labelCurrentPostLikes.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.labelCurrentPostLikes.Name = "labelCurrentPostLikes";
+            this.labelCurrentPostLikes.Size = new System.Drawing.Size(111, 16);
+            this.labelCurrentPostLikes.TabIndex = 60;
+            this.labelCurrentPostLikes.Text = "Likes:";
+            // 
+            // listBoxPages
+            // 
+            this.listBoxPages.FormattingEnabled = true;
+            this.listBoxPages.Location = new System.Drawing.Point(260, 259);
+            this.listBoxPages.Name = "listBoxPages";
+            this.listBoxPages.Size = new System.Drawing.Size(269, 30);
+            this.listBoxPages.TabIndex = 61;
+            // 
+            // listBoxGroups
+            // 
+            this.listBoxGroups.FormattingEnabled = true;
+            this.listBoxGroups.Location = new System.Drawing.Point(17, 260);
+            this.listBoxGroups.Name = "listBoxGroups";
+            this.listBoxGroups.Size = new System.Drawing.Size(204, 30);
+            this.listBoxGroups.TabIndex = 62;
+            // 
+            // listBoxEvents
+            // 
+            this.listBoxEvents.FormattingEnabled = true;
+            this.listBoxEvents.Location = new System.Drawing.Point(260, 305);
+            this.listBoxEvents.Name = "listBoxEvents";
+            this.listBoxEvents.Size = new System.Drawing.Size(269, 30);
+            this.listBoxEvents.TabIndex = 64;
+            // 
+            // listBoxAbout
+            // 
+            this.listBoxAbout.FormattingEnabled = true;
+            this.listBoxAbout.Location = new System.Drawing.Point(600, 88);
+            this.listBoxAbout.Name = "listBoxAbout";
+            this.listBoxAbout.Size = new System.Drawing.Size(150, 43);
+            this.listBoxAbout.TabIndex = 65;
+            // 
+            // listBoxCheckIns
+            // 
+            this.listBoxCheckIns.FormattingEnabled = true;
+            this.listBoxCheckIns.Location = new System.Drawing.Point(17, 305);
+            this.listBoxCheckIns.Name = "listBoxCheckIns";
+            this.listBoxCheckIns.Size = new System.Drawing.Size(204, 30);
+            this.listBoxCheckIns.TabIndex = 66;
+            // 
+            // pictureBoxPage
+            // 
+            this.pictureBoxPage.Location = new System.Drawing.Point(550, 250);
+            this.pictureBoxPage.Name = "pictureBoxPage";
+            this.pictureBoxPage.Size = new System.Drawing.Size(100, 50);
+            this.pictureBoxPage.TabIndex = 67;
+            this.pictureBoxPage.TabStop = false;
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(842, 487);
+            this.Controls.Add(this.pictureBoxPage);
+            this.Controls.Add(this.listBoxCheckIns);
+            this.Controls.Add(this.listBoxAbout);
+            this.Controls.Add(this.listBoxEvents);
+            this.Controls.Add(this.listBoxGroups);
+            this.Controls.Add(this.listBoxPages);
+            this.Controls.Add(this.labelCurrentPostLikes);
+            this.Controls.Add(this.listBoxPostComments);
+            this.Controls.Add(this.listBoxPosts);
+            this.Controls.Add(this.buttonShowAllFriends);
+            this.Controls.Add(this.listBoxFriends);
+            this.Controls.Add(this.pictureBoxProfile);
+            this.Controls.Add(this.pictureBoxCover);
+            this.Controls.Add(this.checkBoxRememberMe);
+            this.Controls.Add(this.buttonLogout);
+            this.Controls.Add(this.buttonLogin);
+            this.Name = "FormMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.FormMain_FormClosed);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxCover)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxProfile)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxPage)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+		#endregion
+
+		private System.Windows.Forms.Button buttonLogin;
+		private System.Windows.Forms.Button buttonLogout;
+        private System.Windows.Forms.CheckBox checkBoxRememberMe;
+        private System.Windows.Forms.PictureBox pictureBoxCover;
+        private System.Windows.Forms.PictureBox pictureBoxProfile;
+        private System.Windows.Forms.ListBox listBoxFriends;
+        private System.Windows.Forms.Button buttonShowAllFriends;
+        private System.Windows.Forms.ListBox listBoxPosts;
+        private System.Windows.Forms.ListBox listBoxPostComments;
+        private System.Windows.Forms.Label labelCurrentPostLikes;
+        private System.Windows.Forms.ListBox listBoxPages;
+        private System.Windows.Forms.ListBox listBoxGroups;
+        private System.Windows.Forms.ListBox listBoxEvents;
+        private System.Windows.Forms.ListBox listBoxAbout;
+        private System.Windows.Forms.ListBox listBoxCheckIns;
+        private System.Windows.Forms.PictureBox pictureBoxPage;
+    }
+}
+
